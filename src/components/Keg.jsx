ee536fa7cc940b77193ed6a2f0f53c85ea995f3d
Dave Sarbora <dsarbora@gmail.com>
2019-04-26 17:01:25 -0700

    make kegs change color in low beer state

--- src/components/Keg.jsx
+++ src/components/Keg.jsx
@@ -1,47 +1,53 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
 class Keg extends Component {
-    constructor(props){
-        super(props);
-        this.state={
-            beersLeft: 125
-        }
-    }
- render() {
-  let color;
-  if (this.state.beersLeft > 30) {
-    color = "green";
-  } else if (this.state.beersLeft > 10) {
-    color = "yellow";
-  } else {
-    color = "red";
+  constructor(props) {
+    super(props);
+    this.state = {
+      beersLeft: 125
+    };
   }
-  return (
-    <div className="frame">
-      <style jsx>{`
-        .frame {
-          width: 200px;
-          height: 200px;
-          border: 1px solid black;
-          padding-top: 10px;
-          text-align: center;
-          margin-top: 10px;
-          margin-right: 10px;
-          background-color: ${color};
-        }
-        .frame:hover {
-          transform: scale(1.05);
-          box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.2);
-        }
-      `}</style>
-      <p>
-        <strong>{props.name}</strong>
-      </p>
-      <p>{props.ABV} ABV</p>
-      <p>${props.price}</p>
-      <button onClick={()=>this.onClickSellButton()} />
-    </div>
-  );
-}
+  onClickSellButton() {
+    this.setState({
+      beersLeft: --this.state.beersLeft
+    });
+    console.log(this.state.beersLeft);
+  }
+  render() {
+    let color;
+    if (this.state.beersLeft > 30) {
+      color = "green";
+    } else if (this.state.beersLeft > 10) {
+      color = "yellow";
+    } else {
+      color = "red";
+    }
+    return (
+      <div className="frame">
+        <style jsx>{`
+          .frame {
+            width: 200px;
+            height: 200px;
+            border: 1px solid black;
+            padding-top: 10px;
+            text-align: center;
+            margin-top: 10px;
+            margin-right: 10px;
+            background-color: ${color};
+          }
+          .frame:hover {
+            transform: scale(1.05);
+            box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.2);
+          }
+        `}</style>
+        <p>
+          <strong>{this.props.name}</strong>
+        </p>
+        <p>{this.props.ABV} ABV</p>
+        <p>${this.props.price}</p>
+        <button onClick={() => this.onClickSellButton()} />
+      </div>
+    );
+  }
 }
 export default Keg;
