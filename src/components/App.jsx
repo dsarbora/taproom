df004352f3cf75b44200efb039463fc7f9463eaa
Dave Sarbora <dsarbora@gmail.com>
2019-04-28 01:59:40 -0700

    create state object in App component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,7 +8,37 @@ import { Switch, Route } from "react-router-dom";
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      Bar: {
+        financials: {
+          income: 0,
+          expenditures: 0,
+          profit: 0,
+          cashOnHand: 200
+        },
+        showing: null,
+        addingNewKeg: false,
+        showingDeleteMenu: false,
+        KegControl: {
+          masterKegList: [
+            {
+              name: "Manny's Pale Ale",
+              ABV: "5.6%",
+              price: "4.50",
+              id: 1,
+              beersLeft: 125
+            }
+          ]
+        }
+      }
+    };
+    this.updateState = this.updateState.bind(this);
+  }
+
+  updateState(componentNameString, newStateObject){
+    this.setState({
+      componentNameString: newStateObject
+    })
   }
 
   render() {
@@ -18,7 +48,11 @@ class App extends Component {
         <Switch>
           <Route exact path="/" component={Home} />
           {/* <Route exact path="/bar/kegs" component={KegControl} /> */}
-          <Route exact path="/bar" component={Bar} />
+          <Route
+            exact
+            path="/bar"
+            component={() => <Bar updateState={this.setState} status={this.state.Bar} />}
+          />
         </Switch>
       </div>
     );
